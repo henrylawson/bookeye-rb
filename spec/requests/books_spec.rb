8715b25464fd7887245bda237094300f996e78f3
Henry Lawson <henry.lawson@foinq.com>
2012-02-13 11:11:14 +0530

    cleaned up specs

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper.rb'
 
 describe "Books" do
-  describe "view home screen" do
-    it "display book editor" do
+  context "when the home screen is viewed" do
+    it "should display book editor title" do
       visit books_path
       page.should have_content 'Editor'
     end
@@ -13,7 +13,7 @@ describe "Books" do
     end
   end
   
-  describe "add/edit/delete operations" do
+  context "when a user accessess index" do
     before(:all) do
       @book = Book.new(
         :title => "REST in Practice: Hypermedia and Systems Architecture",
@@ -25,179 +25,127 @@ describe "Books" do
         :hasEbook => false)
     end
     
-    before(:each) do
-      visit books_path
-      fill_in 'Title', :with => @book.title
-      fill_in 'Author', :with => @book.author
-      fill_in 'Year', :with => @book.year
-      fill_in 'Cover', :with => @book.cover
-      click_button 'Save Book'
-    end
+    context "and does an add with valid data" do
+      before(:each) do
+        visit books_path
+        fill_in 'Title', :with => @book.title
+        fill_in 'Author', :with => @book.author
+        fill_in 'Year', :with => @book.year
+        fill_in 'Cover', :with => @book.cover
+        click_button 'Save Book'
+      end
   
-    it "should add new book" do
-      page.should have_content "REST in Practice: Hypermedia and Systems Architecture"
-      current_path.should == books_path
-      page.should have_content @book.title
-      page.should have_content @book.author
-      page.should have_content @book.year
+      it "should add new book" do
+        page.should have_content "REST in Practice: Hypermedia and Systems Architecture"
+        current_path.should == books_path
+        page.should have_content @book.title
+        page.should have_content @book.author
+        page.should have_content @book.year
+      end
+
+      it "should be able to edit added book" do
+        click_link 'Edit'
+        find_field('Title').value.should == @book.title
+        fill_in 'Title', :with => 'updated book title'
+        click_button 'Save Book'
+        current_path.should == books_path
+        page.should have_content 'updated book title'
+        page.should have_content "Your book was updated"
+      end
+
+      it "should be able to delete added book" do
+        click_link 'Edit'
+        page.should have_content @book.title
+        click_link 'Delete'
+        page.should_not have_content @book.title
+        page.should have_content "The book has been deleted"
+      end
     end
     
-    it "should edit added book" do
-      click_link 'Edit'
-      find_field('Title').value.should == @book.title
-      fill_in 'Title', :with => 'updated book title'
-      click_button 'Save Book'
-      current_path.should == books_path
-      page.should have_content 'updated book title'
-      page.should have_content "Your book was updated"
+    context "and does an add with invalid data" do
+      before(:each) do
+        visit books_path
+        fill_in 'Title', :with => @book.title
+        fill_in 'Author', :with => @book.author
+        fill_in 'Year', :with => @book.year
+        fill_in 'Cover', :with => @book.cover
+      end
+
+      it "should not allow blank title" do
+        fill_in 'Title', :with => ""
+        click_button 'Save Book'
+        page.should have_content "Please ensure your fields are complete"
+      end
+
+      it "should not allow blank author" do
+        fill_in 'Author', :with => ""
+        click_button 'Save Book'
+        page.should have_content "Please ensure your fields are complete"
+      end
+
+      it "should not allow blank year" do
+        fill_in 'Year', :with => ""
+        click_button 'Save Book'
+        page.should have_content "Please ensure your fields are complete"
+      end
+
+      it "should not allow characters in year" do
+        fill_in 'Year', :with => "sdf"
+        click_button 'Save Book'
+        page.should have_content "Please ensure your fields are complete"
+      end
+
+      it "should not allow balnk cover" do
+        fill_in 'Cover', :with => ""
+        click_button 'Save Book'
+        page.should have_content "Please ensure your fields are complete"
+      end
     end
     
-    it "should delete added book" do
-      click_link 'Edit'
-      page.should have_content @book.title
-      click_link 'Delete'
-      page.should_not have_content @book.title
-      page.should have_content "The book has been deleted"
-    end
-  end
-  
-  describe "attempt invalid add operations" do
-    before(:each) do
-      @title = "The best book ever"
-      @author = "Henry Lawson"
-      @year = "2007"
-      @cover = "book.jpg"
-    end
-    
-    it "should not allow blank title" do
-      visit books_path
-      fill_in 'Title', :with => ""
-      fill_in 'Author', :with => @author
-      fill_in 'Year', :with => @year
-      fill_in 'Cover', :with => @cover
-      click_button 'Save Book'
-      page.should have_content "Please ensure your fields are complete"
-      find_field('Title').value.should == ""
-      find_field('Author').value.should == @author
-      find_field('Year').value.should == @year
-      find_field('Cover').value.should == @cover
-    end
-    
-    it "should not allow blank author" do
-      visit books_path
-      fill_in 'Title', :with => @title
-      fill_in 'Author', :with => ""
-      fill_in 'Year', :with => @year
-      fill_in 'Cover', :with => @cover
-      click_button 'Save Book'
-      page.should have_content "Please ensure your fields are complete"
-      find_field('Title').value.should == @title
-      find_field('Author').value.should == ""
-      find_field('Year').value.should == @year
-      find_field('Cover').value.should == @cover
-    end
-    
-    it "should not allow blank year" do
-      visit books_path
-      fill_in 'Title', :with => @title
-      fill_in 'Author', :with => @author
-      fill_in 'Year', :with => ""
-      fill_in 'Cover', :with => @cover
-      click_button 'Save Book'
-      page.should have_content "Please ensure your fields are complete"
-      find_field('Title').value.should == @title
-      find_field('Author').value.should == @author
-      find_field('Year').value.should == ""
-      find_field('Cover').value.should == @cover
-    end
-    
-    it "should not allow characters in year" do
-      visit books_path
-      fill_in 'Title', :with => @title
-      fill_in 'Author', :with => @author
-      fill_in 'Year', :with => "sdf"
-      fill_in 'Cover', :with => @cover
-      click_button 'Save Book'
-      page.should have_content "Please ensure your fields are complete"
-      find_field('Title').value.should == @title
-      find_field('Author').value.should == @author
-      find_field('Year').value.should == "sdf"
-      find_field('Cover').value.should == @cover
-    end
-    
-    it "should not allow balnk cover" do
-      visit books_path
-      fill_in 'Title', :with => @title
-      fill_in 'Author', :with => @author
-      fill_in 'Year', :with => @year
-      fill_in 'Cover', :with => ""
-      click_button 'Save Book'
-      page.should have_content "Please ensure your fields are complete"
-      find_field('Title').value.should == @title
-      find_field('Author').value.should == @author
-      find_field('Year').value.should == @year
-      find_field('Cover').value.should == ""
-    end
-  end
-  
-  describe "attempt invalid edit operations" do
-    before(:each) do
-      @title = "The best book ever"
-      @author = "Henry Lawson"
-      @year = "2007"
-      @cover = "book.jpg"
-      visit books_path
-      fill_in 'Title', :with => @title
-      fill_in 'Author', :with => @author
-      fill_in 'Year', :with => @year
-      fill_in 'Cover', :with => @cover
-      click_button 'Save Book'
-      click_link 'Edit'
-    end
-    
-    it "should not allow blank title" do
-      fill_in 'Title', :with => ''
-      click_button 'Save Book'
-      page.should have_content "There was an error updating your book"
-    end
-    
-    it "should not allow blank author" do
-      fill_in 'Author', :with => ''
-      click_button 'Save Book'
-      page.should have_content "There was an error updating your book"
-    end
-    
-    it "should not allow blank year" do
-      fill_in 'Year', :with => ''
-      click_button 'Save Book'
-      page.should have_content "There was an error updating your book"
-    end
-    
-    it "should not allow characters in year" do
-      fill_in 'Year', :with => 'sdf'
-      click_button 'Save Book'
-      
-      page.should have_content "There was an error updating your book"
-    end
-  end
-  
-  describe "given user wants to use search" do 
-    it "should be able to enter search" do
-      visit books_path
-      searchFieldName = 'Search'
-      searchTerm = 'My awesome book'
-      fill_in searchFieldName, :with => searchTerm
-      find_field(searchFieldName).value.should == searchTerm
-    end
-    
-    
-    #it "should have correct results" do
-      #visit books_path
-      #fill_in 'Search', :with => 'Agile Samruai'
-      #click_link 'Pull'
-      #find_field('Title').value.should == 'The Agile Samurai: How Agile Masters Deliver Great Software'
-      #find_field('Author').value.should == 'Jonathan Rasmusson'
-      #find_field('Year').value.should == '2010'
-    #end
+    context "and does an edit with invalid data" do
+       before(:each) do
+         visit books_path
+         fill_in 'Title', :with => @book.title
+         fill_in 'Author', :with => @book.author
+         fill_in 'Year', :with => @book.year
+         fill_in 'Cover', :with => @book.cover
+         click_button 'Save Book'
+         click_link 'Edit'
+       end
+
+       it "should not allow blank title" do
+         fill_in 'Title', :with => ''
+         click_button 'Save Book'
+         page.should have_content "There was an error updating your book"
+       end
+
+       it "should not allow blank author" do
+         fill_in 'Author', :with => ''
+         click_button 'Save Book'
+         page.should have_content "There was an error updating your book"
+       end
+
+       it "should not allow blank year" do
+         fill_in 'Year', :with => ''
+         click_button 'Save Book'
+         page.should have_content "There was an error updating your book"
+       end
+
+       it "should not allow characters in year" do
+         fill_in 'Year', :with => 'sdf'
+         click_button 'Save Book'
+         page.should have_content "There was an error updating your book"
+       end
+     end
+     
+     context "and trys to search" do 
+       it "should be able to enter search" do
+         visit books_path
+         searchFieldName = 'Search'
+         searchTerm = 'My awesome book'
+         fill_in searchFieldName, :with => searchTerm
+         find_field(searchFieldName).value.should == searchTerm
+       end
+     end
   end
 end
